b137cea5af9fecddd848b3f68324132bdb5143c7
Robert Bryan <bryarobert@gmail.com>
2017-08-09 13:08:18 -0400

    .

--- config/scout.php
+++ config/scout.php
@@ -71,8 +71,8 @@ return [
     */
 
     'algolia' => [
-        'id' => env('ALGOLIA_APP_ID', 'LQQZ4DIY7M'),
-        'secret' => env('ALGOLIA_SECRET', '756ed1a67735b2a8c36ed78e42c6ea78'),
+        'id' => env('ALGOLIA_APP_ID', 'FUNGFC5XK6'),
+        'secret' => env('ALGOLIA_SECRET', '61655e24b64beb71733876fef4a6355a'),
     ],
 
 ];
